8c2dc2c0ca2490107c453842e4e285830c62a175
Manikant Kumar <mani2474695@gmail.com>
2021-05-25 17:49:11 +0530

    Delete .buildozer directory

--- .buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/devil/work/kivy/galaxy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/devil/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/devil/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/devil/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
